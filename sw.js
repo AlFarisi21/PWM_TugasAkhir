16dd3c4620edfb3043824515a562cd63954989e5
Muhammad Aurelia Mada Al-Farisi <144197420+AlFarisi21@users.noreply.github.com>
2025-06-30 19:09:41 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,16 +5,16 @@ const assets = [
   '/PWM_TugasAkhir/detail.html',
   '/PWM_TugasAkhir/manifest.json',
   '/PWM_TugasAkhir/logoblog(1).png',
-  '/PWM_TugasAkhir/logoblog-192.png',
-  '/PWM_TugasAkhir/logoblog-512.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
 ];
 
 self.addEventListener('install', event => {
-  self.skipWaiting(); // langsung aktif
+  self.skipWaiting();
   event.waitUntil(
     caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
+      return cache.addAll(assets).catch(err => {
+        console.error('Cache addAll failed:', err);
+      });
     })
   );
 });
