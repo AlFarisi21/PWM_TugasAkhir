d8b69138fe3a729825a871be5e0fe4a7e3119e8c
Muhammad Aurelia Mada Al-Farisi <144197420+AlFarisi21@users.noreply.github.com>
2025-06-30 19:25:24 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,18 @@
 const cacheName = 'blog-tech-cache-v1';
 const assets = [
-  '/PWM_TugasAkhir/',
-  '/PWM_TugasAkhir/index.html',
-  '/PWM_TugasAkhir/detail.html',
-  '/PWM_TugasAkhir/manifest.json',
-  '/PWM_TugasAkhir/logoblog(1).png',
+  './',
+  './index.html',
+  './detail.html',
+  './manifest.json',
+  './logoblog(1).png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
 ];
 
 self.addEventListener('install', event => {
-  self.skipWaiting();
+  self.skipWaiting(); // langsung aktif
   event.waitUntil(
     caches.open(cacheName).then(cache => {
-      return cache.addAll(assets).catch(err => {
-        console.error('Cache addAll failed:', err);
-      });
+      return cache.addAll(assets);
     })
   );
 });
